8056051088b96ac2b2eddfba9d4c40f91e4a4fd1
x.bling <48610967+blessingmwalein@users.noreply.github.com>
2022-03-29 11:13:20 +0200

    Update Transaction.js

--- src/Transaction.js
+++ src/Transaction.js
@@ -1,22 +1,27 @@
-const crypto = require('crypto');
+const SHA256 = require('crypto-js/sha256');
 const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
 
+
 class Transaction {
-    constructor(fromAddress , toAddress, details, amount){
+    constructor(fromAddress, toAddress, details, amount) {
         this.fromAddress = fromAddress;
         this.toAddress = toAddress;
         this.details = details;
         this.amount = amount;
         this.timestamp = Date.now();
+        // this.certificateHash = this.detailsHash();
     }
 
-    calculateHash(){
-       return crypto.createHash('sha256').update(this.fromAddress + this.toAddress + JSON.stringify(this.details) + this.timestamp + this.amount).digest('hex');
+    calculateHash() {
+        return SHA256(this.fromAddress + this.toAddress + JSON.stringify(this.details) + this.timestamp + this.amount).toString();
     }
 
-    signTransaction(signingKey){
-        if(signingKey.getPublic('hex') !== this.fromAddress){
+    detailsHash(){
+        return SHA256(JSON.stringify(this.details)).toString();
+    }
+    signTransaction(signingKey) {
+        if (signingKey.getPublic('hex') !== this.fromAddress) {
             throw new Error('You can not sign this transaction');
         }
 
@@ -25,17 +30,27 @@ class Transaction {
 
         this.signature = sig.toDER('hex');
     }
+    signDetails(signingKey){
+        if (signingKey.getPublic('hex') !== this.fromAddress) {
+            throw new Error('You can not sign this transaction');
+        }
+        const deHash = this.detailsHash();
+        const sign = signingKey.sign(deHash, 'base64');
 
-    isValid(){
-        if(this.fromAddress === null) return true;
+        this.detailsSignature = sign.toDER('hex');
+        return this.detailsSignature;
+    }
 
-        if(!this.signature || this.signature.length === 0){
+    isValid() {
+        if (this.fromAddress === null) return true;
+
+        if (!this.signature || this.signature.length === 0) {
             throw new Error('No signature in this tranaction');
         }
         const publicKey = ec.keyFromPublic(this.fromAddress, 'hex');
         return publicKey.verify(this.calculateHash(), this.signature);
     }
-    
+
 }
 
 module.exports.Transaction = Transaction;
